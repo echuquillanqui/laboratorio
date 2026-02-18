a9819fe2663b8d807422de91ca084e4d78c6ddda
echuquillanqui <echuquillanquiy@gmail.com>
2026-02-18 17:16:16 -0500

    corrigiendo errores

--- resources/views/atenciones/cashbox/index.blade.php
+++ resources/views/atenciones/cashbox/index.blade.php
@@ -2,7 +2,6 @@
 
 @section('content')
 <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.3/font/bootstrap-icons.min.css">
-<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.bundle.min.js"></script>
 
 <style>
     .content-compact { font-size: 0.9rem; }
